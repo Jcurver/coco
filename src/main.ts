547084280df37fcdb2f304ddc74a0105e9a3aa06
ifizzyou <pjh15237213@gmail.com>
2023-06-11 00:19:45 +0900

    change port

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import * as config from 'config';
+// import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  const serverConfig = config.get('server');
+  // const serverConfig = config.get('server');
 
-  const port = serverConfig.port;
+  const port = 5000;
+
   await app.listen(port);
   logger.log(`💡Application listening on port ${port}`);
 }
