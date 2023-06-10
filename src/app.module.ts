4ba36b0d20959b23eea44de7971e8c409f999456
ifizzyou <pjh15237213@gmail.com>
2023-06-11 02:28:21 +0900

    change port

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,26 @@ import { BoardsService } from './boards/boards.service';
 import { BoardsModule } from './boards/boards.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { User } from './auth/user.entity';
+import { Board } from './boards/board.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: '.development.env',
     }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [__dirname + 'dist/**/*.entity.{js,ts}', User, Board],
+      // synchronize: dbConfig.synchronize,
+      synchronize: true,
+    }),
     BoardsModule,
     AuthModule,
   ],
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,16 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Board } from 'src/boards/board.entity';
-import * as config from 'config';
+import * as config from '@nestjs/config';
 
-const dbConfig = config.get('db');
+// const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  username: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
+  port: +process.env.MYSQL_PORT,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: dbConfig.synchronize,
+  // synchronize: dbConfig.synchronize,
+  synchronize: true,
 };
